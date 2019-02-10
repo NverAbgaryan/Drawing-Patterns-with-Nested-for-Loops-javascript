a861f2e3774ab9aae402bd6ab24343a2a7953505
nverabgaryan <abgaryannver1993@gmail.com>
2019-02-10 19:01:43 +0400

    Add Diamond of Asterisks loop

--- index.js
+++ index.js
@@ -14,11 +14,14 @@
 
 ----------------*/
 
+let y = 10;
+let x = 10;
+
 let str = "";
 
-for(let i = 1; i < 10; i++ ){
-    for(let j = 1; j < 10; j++){
-        if(i + j >= 10){
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
+        if(i + j >= y){
             str = str.concat("*");
         }else{
             str = str.concat(" ")
@@ -49,8 +52,8 @@ console.log(str)
 
 let str2 = "";
 
-for(let i = 1; i < 10; i++ ){
-    for(let j = 1; j < 10; j++){
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
         if(i <= j ){
             str2 = str2.concat("*");
         }else{
@@ -81,8 +84,8 @@ console.log(str2)
 
 let str3 = "";
 
-for(let i = 1; i < 10; i++ ){
-    for(let j = 1; j < 10; j++){
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
         if(i >= j ){
             str3 = str3.concat("*");
         }
@@ -108,8 +111,8 @@ console.log(str3)
  ---------------------------*/
 let str4 = "";
 
-for(let i = 1; i < 10; i++ ){
-    for(let j = 1; j < 10; j++){
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
         if( j >= i ){
             str4 = str4.concat("*");
         }
@@ -118,3 +121,39 @@ for(let i = 1; i < 10; i++ ){
 }
 
 console.log(str4)
+
+/**--------------------
+ Diamond of Asterisks
+
+     *
+    ***
+   *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+
+
+ ---------------------*/
+
+let str5 = "";
+
+for(let i = 1; i < y; i++ ){
+    for(let j = 1; j < x; j++){
+        if(i <= y / 2 && j >= (y / 2) - (i - 1) && j <= (y / 2) + (i - 1) ){
+            str5 = str5.concat("*");
+        }else if(i >= y / 2
+          && j > ((y / 2) -  i) * (-1)
+          && j < (y - ((y / 2) -  i) * (-1))){
+            str5 = str5.concat("*");
+        }
+        else {
+            str5 = str5.concat(" ");
+        }
+    }
+    str5 = str5.concat("\n");
+}
+
+console.log(str5)
